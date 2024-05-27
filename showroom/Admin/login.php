b31a0d1efa9a00bb1857750f8e190a18fea7812c
203040004 <hasbulah.203040004@mail.unpas.ac.id>
2024-05-27 11:48:21 +0700

    membuat halaman login

---
+++ showroom/Admin/login.php
@@ -0,0 +1,84 @@
+<?php 
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ARJUNA SHOWROOM</title>
+    <!-- Import Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+    <!-- Custom CSS -->
+    <style>
+        body {
+            background-image: url('');
+            background-size: cover;
+        }
+        .login-container {
+            margin-top: 100px;
+        }
+        .login-card {
+            padding: 20px;
+        }
+    </style>
+</head>
+
+<body>
+    <?php if (isset($error)) : ?>
+        <p style="color: red; font-style: italic;">Username atau Password salah</p>
+    <?php endif; ?>
+
+    <!-- Login Section -->
+    <section id="Log In" class="login-container">
+        <div class="container">
+            <div class="row justify-content-center">
+                <div class="col-md-6">
+
+                    <form action="" method="POST">
+                        <div class="card login-card">
+                            <div class="card-body">
+                                <h5 class="card-title text-center">Log In</h5>
+                                <div class="mb-3">
+                                    <label for="username" class="form-label">Username</label>
+                                    <input type="text" name="username" id="username" class="form-control" required autocomplete="off">
+                                </div>
+
+                                <div class="mb-3">
+                                    <label for="password" class="form-label">Password</label>
+                                    <input type="password" name="password" id="password" class="form-control" required autocomplete="off">
+                                </div>
+
+                                <div class="form-check mb-3">
+                                    <input type="checkbox" name="remember" class="form-check-input" id="remember">
+                                    <label class="form-check-label" for="remember">Remember</label>
+                                </div>
+
+                                <button type="submit" name="submit" class="btn btn-primary w-100">Log In</button>
+
+                                <p class="mt-3">
+                                    Belum punya akun? registrasi <a href="registrasi.php">Disini</a>
+                                </p>
+
+                                <button type="button" class="btn btn-secondary w-100 mt-2" onclick="location.href='../index.php'">
+                                    Kembali
+                                </button>
+                            </div>
+                        </div>
+                    </form>
+
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <!-- Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-beta2/js/bootstrap.min.js"></script>
+</body>
+
+</html>
